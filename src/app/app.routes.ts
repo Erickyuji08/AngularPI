5e17639a3340b361b5856bf240eefc266014cae0
ERICK YUJI CONCEICAO <erick.yconceicao@senacsp.edu.br>
2025-10-07 22:06:00 -0300

    Adicionado route pessoas/incluir

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,22 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { PessoaLogin } from './pages/pessoa-login/pessoa-login';
+import { PessoaListagemComponent } from './pages/pessoa-listagem/pessoa-listagem';
+import { PessoaForm } from './pages/pessoa-form/pessoa-form';
 
 export const routes: Routes = [
     {path:'', component: Home},
-    {path:'pessoas/login', component: PessoaLogin}
+    {path:'pessoas/login', component: PessoaLogin},
+
+    {
+     path: 'pessoas',
+     component: PessoaListagemComponent,
+     title: 'Lista de pessoas'
+    },
+
+    {
+     path: 'pessoas/incluir',
+     component: PessoaForm,
+     title: 'Pessoa form'
+    }
 ];
